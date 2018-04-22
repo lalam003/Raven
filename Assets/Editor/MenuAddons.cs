29a006c3a4bf2bff9ef1ba90fb6e369c1c9ecc89
ddiep003@ucr.edu <ddiep003@ucr.edu>
2018-04-21 23:38:04 -0700

    Setup a few things.
    Created my own scene to work in.
    Create Resources folder for Json files.
    Created Editor folder for editor extensions.
    Created ReadJson CS script
    Created MenuAddons CS script for editor extension.

---
+++ Assets/Editor/MenuAddons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuAddons {
+
+    // Use this for initialization
+    [MenuItem("Tools/Clear PlayerPrefs")]
+    private static void NewMenuOption()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+    [MenuItem("Assets/Create/Create Json")]
+    private static void CreateJson()
+    {
+        // Create and add a new default Json for testing and other things.
+    }
+}
---
+++ Assets/Scripts/DannyScripts/ReadJson.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class ReadJson : MonoBehaviour {
+
+    string jsonString;
+	// Use this for initialization
+	void Start () {
+        jsonString = File.ReadAllText(Application.dataPath + "/Resources/Readthis.txt");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
