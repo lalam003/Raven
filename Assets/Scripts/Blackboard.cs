afb0c64401f63f8be3ec240e3561412c7f822b47
nadnbuds <lalam003@ucr.edu>
2018-04-23 00:33:18 -0700

    Implemented Player Movement

--- Assets/Scripts/Blackboard.cs
+++ Assets/Scripts/Blackboard.cs
@@ -3,4 +3,14 @@
 public static class Blackboard
 {
     public static PlayerControls ControlMap;
+
+    public static PlayerControls ParseControlMap(PlayerControls newControls)
+    {
+        if(ControlMap != null)
+        {
+            return ControlMap;
+        }
+
+        return newControls;
+    }
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,10 +1,52 @@
 ﻿using UnityEngine;
 
-public class PlayerControls : MonoBehaviour
+public class PlayerControls
 {
-    KeyCode North = KeyCode.W;
-    KeyCode South = KeyCode.S;
-    KeyCode East  = KeyCode.A;
-    KeyCode West  = KeyCode.D;
-    KeyCode Interact = KeyCode.Space;
+    public enum Controls
+    {
+        North = 0,
+        South,
+        East,
+        West,
+        Interact
+    }
+
+    public KeyCode North { get; private set; }
+    public KeyCode South { get; private set; }
+    public KeyCode East { get; private set; }
+    public KeyCode West { get; private set; }
+    public KeyCode Interact { get; private set; }
+
+    public void InitializeDefaults()
+    {
+        North    = KeyCode.W;
+        South    = KeyCode.S;
+        East     = KeyCode.A;
+        West     = KeyCode.D;
+        Interact = KeyCode.Space;
+    }
+
+    public void SetKey(Controls control, KeyCode newInput)
+    {
+        switch (control)
+        {
+            case Controls.North:
+                North = newInput;
+                break;
+            case Controls.South:
+                South = newInput;
+                break;
+            case Controls.East:
+                East = newInput;
+                break;
+            case Controls.West:
+                West = newInput;
+                break;
+            case Controls.Interact:
+                Interact = newInput;
+                break;
+            default:
+                break;
+        }
+    }
 }
