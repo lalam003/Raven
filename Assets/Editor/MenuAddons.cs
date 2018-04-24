c9588ac1c24a7427b798606122123e9f5e1ec11d
ddiep003@ucr.edu <ddiep003@ucr.edu>
2018-04-24 01:57:14 -0700

    Revert "Create Json in Unity Editor"
    This reverts commit 46936336e3c1e3f003c3670fbd8b0e4de37f2922.

--- Assets/Editor/MenuAddons.cs
+++ Assets/Editor/MenuAddons.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using LightJson;
-using System.IO;
+
 public class MenuAddons {
 
     // Use this for initialization
@@ -16,24 +15,5 @@ public class MenuAddons {
     private static void CreateJson()
     {
         // Create and add a new default Json for testing and other things.
-        var json = new JsonObject()
-        .Add("menu", new JsonArray()
-        .Add("home")
-        .Add("projects")
-        .Add("about")
-        ).ToString(true);
-        string str = json.ToString();
-        string path = Application.dataPath + "/Resources/BaseJson.json";
-        using (FileStream fs = new FileStream(path, FileMode.Create))
-        {
-            using (StreamWriter writer = new StreamWriter(fs))
-            {
-                writer.Write(str);
-            }
-        }
-        #if UNITY_EDITOR
-        UnityEditor.AssetDatabase.Refresh();
-        #endif
-
     }
 }
