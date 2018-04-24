ee392a5a7b1fb3c4a60d755da014c15040655da9
ddiep003@ucr.edu <ddiep003@ucr.edu>
2018-04-24 02:00:47 -0700

    Create Json in Editor

--- Assets/Editor/MenuAddons.cs
+++ Assets/Editor/MenuAddons.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-
+using LightJson;
+using System.IO;
 public class MenuAddons {
 
     // Use this for initialization
@@ -14,6 +15,22 @@ public class MenuAddons {
     [MenuItem("Assets/Create/Create Json")]
     private static void CreateJson()
     {
+        var json = new JsonObject()
+        .Add("menu", new JsonArray()
+        .Add("home")
+        .Add("projects")
+        .Add("about")
+        ).ToString(true);
+
         // Create and add a new default Json for testing and other things.
+        string datapath = Application.dataPath + "/Resources/BaseJson.json";
+        using (FileStream fstream = new FileStream(datapath, FileMode.Create))
+        {
+            using (StreamWriter swriter = new StreamWriter(fstream))
+            {
+                swriter.Write(json);
+            }
+        }
+        UnityEditor.AssetDatabase.Refresh();
     }
 }
