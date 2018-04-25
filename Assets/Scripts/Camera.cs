41729449514632de18dbea0b5f515677719c1553
nadnbuds <lalam003@ucr.edu>
2018-04-24 23:23:53 -0700

    Camera/Room System

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : Singleton<Camera>
+{
+    [SerializeField]
+    private float cameraTransitionSpeed = 1.0f;
+
+    private IEnumerator currentRoutine;
+    
+    public bool MoveCamera(Vector3 position)
+    {
+        if(currentRoutine != null && !currentRoutine.MoveNext())
+        {
+            currentRoutine = move(position);
+            StartCoroutine(currentRoutine);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    IEnumerator move(Vector3 endPos)
+    {
+        Vector3 startPos = transform.position;
+        Vector3 currentPos = startPos;
+        float curr = 0.0f;
+
+        yield return null;
+        while(currentPos != endPos)
+        {
+            curr += Time.deltaTime * cameraTransitionSpeed;
+            currentPos = Vector3.Lerp(startPos, endPos, curr / 1.0f);
+            transform.position = currentPos;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Room : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 cameraFocalPoint;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!collision.gameObject.GetComponent<PlayerMovement>().Equals(null))
+        {
+            Camera.Instance.MoveCamera(cameraFocalPoint);
+        }
+    }
+}
