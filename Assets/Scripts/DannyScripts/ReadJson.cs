4ef7f9bc2280f711af68922d8de9b794b2bcd03f
ddiep003@ucr.edu <ddiep003@ucr.edu>
2018-04-24 21:33:07 -0700

    Json File Parser

--- Assets/Scripts/DannyScripts/ReadJson.cs
+++ Assets/Scripts/DannyScripts/ReadJson.cs
@@ -2,16 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
+using LightJson;
+using LightJson.Serialization;
+using System.Reflection;
+
 public class ReadJson : MonoBehaviour {
 
-    string jsonString;
-	// Use this for initialization
-	void Start () {
-        jsonString = File.ReadAllText(Application.dataPath + "/Resources/Readthis.txt");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Use this for initialization
+    JsonObject jsonFile;
+    RectTransform canvasRect;
+    Text t;
+    JsonArray values;
+    int next = 0;
+    void Start () {
+        jsonFile = new JsonObject();
+        string datapath = Application.dataPath + "/Resources/BaseJson.json";
+        jsonFile = JsonReader.ParseFile(datapath);
+
+        string json = jsonFile.ToString();
+        Debug.Log(json);
+        t = this.GetComponent<Text>();
+        values = JsonValue.Parse(json)["dialogue"].AsJsonArray;
+        Debug.Log(values);
+        //t.GetComponent<TextGenerationSettings>();
+        CheckTextOverFlow();
+    }
+    private void Update()
+    {
+        //CheckTextOverFlow();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ScrollText();
+        }
+    }
+    void ScrollText()
+    {
+        if(next >= values.Count)
+        {
+            Debug.Log("End of Dialogue");
+            return;
+        }
+        t.text = values[next];
+        next++;
+    }
+    bool CheckTextOverFlow()
+    {
+        float textboxwidth = t.preferredWidth;
+        float textboxheight = t.preferredHeight;
+        TextGenerator gen = t.cachedTextGenerator;
+        Debug.Log("Pref Height: " + textboxheight);
+        Debug.Log("Pref Width: " + textboxwidth);
+        //float canvaswidth = canvasRect.GetComponentInParent<RectTransform>();
+        return false;
+    }
 }
