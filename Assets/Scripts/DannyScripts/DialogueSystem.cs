f036e3f99a1caa7abefbf20e31cecd515df41191
ddiep003@ucr.edu <ddiep003@ucr.edu>
2018-05-01 18:55:21 -0700

    Made Requested Changes and some Updates

--- Assets/Scripts/DannyScripts/DialogueSystem.cs
+++ Assets/Scripts/DannyScripts/DialogueSystem.cs
@@ -2,34 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueSystem : MonoBehaviour {
+public class DialogueSystem : MonoBehaviour
+{
 
     /* Dialogue System attached to interactable objects such as NPCS or anything that need a text box.
-     * OnTriggerStay will only allow interaction if player is within range of object. 
+     * OnTriggerStay will only allow interaction if player is within range of object.
+     * Needs testing with overlapping triggers.
      */
-    bool isInteracting = false;
+    private bool isInteracting = false;
     [SerializeField]
-    GameObject DialogueBox;
+    private GameObject DialogueBox;
     [SerializeField]
-    string JsonFile, Subsection;
-	void Start () {
-        //Search by tag or name or some kind of lookup if using serialize field to get a reference isnt to your taste
-		//DialogueBox = GameObject.Find()
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private string JsonFile, Subsection;
+
+
+    private void Start()
+    {
+        if (DialogueBox == null)
+        {
+            Debug.LogError("DialogueBox Missing");
+        }
+    }
+
+
+
+    // Update is called once per frame
+    private void Update ()
+    {
 		if(DialogueBox.activeSelf == false)
         {
             isInteracting = false;
         }
 	}
-    void InjectDialogue()
+    //Returns current state of interaction
+    public bool GetStatus()
+    {
+        return isInteracting;
+    }
+    //Specifies which Json and which section to read from
+    private void InjectDialogue()
     {
         ReadJson dialogue = DialogueBox.GetComponent<ReadJson>();
         if(dialogue == null)
         {
             Debug.Log("Failed to get Json data");
+
             return;
         }
         else
--- Assets/Scripts/DannyScripts/ReadJson.cs
+++ Assets/Scripts/DannyScripts/ReadJson.cs
@@ -7,7 +7,8 @@ using LightJson;
 using LightJson.Serialization;
 using System.Reflection;
 //Author Danny Diep
-public class ReadJson : MonoBehaviour {
+public class ReadJson : MonoBehaviour
+{
 
     /* Attach this script to a Text UI object
      * Specify the text to be loaded in the fileToRead field 
@@ -19,29 +20,25 @@ public class ReadJson : MonoBehaviour {
 
 
     // Use this for initialization
-    JsonObject jsonFile;
-    RectTransform canvasRect;
-    Text t;
-    JsonArray values;
-    int next = 0;
+    private JsonObject jsonFile;
+    private Text t;
+    private JsonArray values;
+    private int next = 0;
     [SerializeField]
-    string fileToRead = "BaseJson.json";
+    public string fileToRead = "BaseJson.json";
     [SerializeField]
-    string subsection = "dialogue";
+    private string subsection = "dialogue";
     [SerializeField]
-    float textSpeed = 0.01f;
-    //float slowSpeed = 0.3f;
-    //float fastSpeed = 0.01f;
-    bool coroutineRunning = false;
+    private float textSpeed = 0.01f;
+    private bool coroutineRunning = false;
 
-    void Start () {
+    private void Start ()
+    {
         jsonFile = new JsonObject();
         string datapath = Application.dataPath + "/Resources/" + fileToRead;
         jsonFile = JsonReader.ParseFile(datapath);
-
-        string json = jsonFile.ToString();
         t = this.GetComponentInChildren<Text>();
-        values = JsonValue.Parse(json)[subsection].AsJsonArray;
+        values = JsonValue.Parse(jsonFile.ToString())[subsection].AsJsonArray;
         ScrollText();
     }
     public void Reset()
@@ -49,12 +46,15 @@ public class ReadJson : MonoBehaviour {
         t.text = "";
         next = 0;
     }
+    public void SetTextSpeed(float lettersPerSecond)
+    {
+        textSpeed = lettersPerSecond;
+    }
     public void SetParameters(string newFile = "BaseJson.json", string newSubsection = "dialogue")
     {
         string datapath = Application.dataPath + "/Resources/" + newFile;
         jsonFile = JsonReader.ParseFile(datapath);
-        string json = jsonFile.ToString();
-        values = JsonValue.Parse(json)[subsection].AsJsonArray;
+        values = JsonValue.Parse(jsonFile.ToString())[subsection].AsJsonArray;
     }
     private void OnEnable()
     {
@@ -63,20 +63,18 @@ public class ReadJson : MonoBehaviour {
     }
     private void Update()
     {
-        //CheckTextOverFlow();
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Update 1 Space");
             if(coroutineRunning == false)
             {
-                Debug.Log("Update 2 Space");
                 ScrollText();
             }
         }
     }
-    IEnumerator PrintText(int index)
+    private IEnumerator PrintText(int index)
     {
         yield return null;
+
         string sentence = values[index];
         string output = "";
         foreach (char word in sentence )
@@ -84,9 +82,9 @@ public class ReadJson : MonoBehaviour {
             output += word;
             if(Input.GetKeyDown(KeyCode.Space))
             {
-                Debug.Log("Print now");
                 t.text = sentence;
                 coroutineRunning = false;
+
                 yield break;
             }
             else
@@ -96,16 +94,16 @@ public class ReadJson : MonoBehaviour {
             }
                      
         }
-        Debug.Log("Finished Printing");
         coroutineRunning = false;
     }
     
-    void ScrollText()
+    private void ScrollText()
     {
         if(next >= values.Count)
         {
             Reset();
             this.gameObject.SetActive(false);
+
             return;
         }
         else
@@ -116,14 +114,12 @@ public class ReadJson : MonoBehaviour {
         }
         
     }
-    bool CheckTextOverFlow()
+    private bool CheckTextOverFlow()
     {
         float textboxwidth = t.preferredWidth;
         float textboxheight = t.preferredHeight;
         TextGenerator gen = t.cachedTextGenerator;
-        Debug.Log("Pref Height: " + textboxheight);
-        Debug.Log("Pref Width: " + textboxwidth);
-        //float canvaswidth = canvasRect.GetComponentInParent<RectTransform>();
+
         return false;
     }
 }
