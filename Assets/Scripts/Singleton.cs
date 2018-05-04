a763eaac3e97ecde7fa01a9aade7b0ef08d37d68
nadnbuds <lalam003@ucr.edu>
2018-05-03 22:45:18 -0700

    Fixed Singleton Issue

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -11,11 +11,22 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         get
         {
             if (instance == null)
-            {  
-                Debug.LogError("Object not found. Creating new object...");
-                singleton = new GameObject();
-                instance = singleton.AddComponent<T>();
-                singleton.name = typeof(T).ToString();
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+
+                if(instance == null)
+                {
+                    Debug.LogError("Object not found. Creating new object...");
+                    singleton = new GameObject();
+                    instance = singleton.AddComponent<T>();
+                    singleton.name = typeof(T).ToString();
+                }
+
+                else
+                {
+                    singleton = instance.gameObject;
+                }
+
             }
 
             return instance;
@@ -24,11 +35,17 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 
     public static GameObject GameObject
     {
-        get { return Instance.gameObject; }
+        get
+        {
+            return Instance.gameObject;
+        }
     }
 
     public static Transform Transform
     {
-        get { return Instance.transform; }
+        get
+        {
+            return Instance.transform;
+        }
     }
 }
