30be01d21e0c0f8123dde985b134e9e0d92abcfb
nadnbuds <lalam003@ucr.edu>
2018-05-08 20:25:40 -0700

    Added Interactable Template -added Event Template

---
+++ Assets/Scripts/Event.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Event : ScriptableObject
+{
+    public enum EventType
+    {
+        Time = 0,
+        Interact,
+        Trigger
+    }
+
+    [SerializeField]
+    private EventType type;
+
+    [SerializeField]
+    private EventTask task;
+}
--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -2,6 +2,27 @@
 
 public abstract class Interactable : MonoBehaviour
 {
-    public abstract void Interact();
+    private static Callback onInteract;
+    public static Callback OnInteract
+    {
+        get
+        {
+            return onInteract;
+        }
+        set
+        {
+            if(value != null)
+            {
+                onInteract = value;
+            }
+            else
+            {
+                onInteract = () => { };
+            }
+        }
+    }
 
+    public abstract void Interact();
 }
+
+public delegate void Callback();
