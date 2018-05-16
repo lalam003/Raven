23569978231f7fab1d744109e9eae80ce4791a3f
ddiep003@ucr.edu <ddiep003@ucr.edu>
2018-05-15 20:25:37 -0700

    Save and Load
    PlayerData is the serializable class with all the data saved to a savegame.json.

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public static PlayerData currentPlayer;
+    public Transform currentPosition;
+    public Inventory currentInventory;
+    //Itemdata
+    //Events
+
+    public PlayerData()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+using LightJson;
+using LightJson.Serialization;
+public static class SaveLoad
+{
+    public static List<PlayerData> saveData = new List<PlayerData>();
+    public static PlayerData loadData = new PlayerData();
+    public static void SaveGame()
+    {
+        string json = JsonUtility.ToJson(PlayerData.currentPlayer);
+        saveData.Add(PlayerData.currentPlayer);
+        string dataPath = Application.persistentDataPath + "/savegame.json";
+        using (FileStream fstream = new FileStream(dataPath, FileMode.Create))
+        {
+            using (StreamWriter swriter = new StreamWriter(fstream))
+            {
+                swriter.Write(json);
+            }
+        }
+    }
+    public static void LoadGame()
+    {
+        if(File.Exists(Application.persistentDataPath + "/savedgame.json"))
+        {
+            JsonObject data = ReadJson.ParseJsonFile(Application.persistentDataPath + "/savedgame.json");
+            loadData = JsonUtility.FromJson<PlayerData>(data.ToString());
+        }
+    }
+}
